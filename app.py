019385849f38b9c5890faf273f392012e13c52e5
pavanraje234 <pavanrajeg234@gmail.com>
2025-03-25 11:05:52 +0530

    added port

--- app.py
+++ app.py
@@ -235,5 +235,7 @@ def result():
     return render_template('result.html')
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
     
